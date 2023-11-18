089a2ca8cb6fb4611f25acff238d0d628232e986
YongNyeo <won05121@gmail.com>
2023-11-18 13:01:27 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/sports/SportsApplication.java
@@ -0,0 +1,16 @@
+package com.example.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsApplication.class, args);
+
+	}
+
+}
